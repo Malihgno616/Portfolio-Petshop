39499dc33485175641292b411d0acbee36c0ece4
ackindev <ackinlino123@gmail.com>
2025-01-15 13:59:56 -0300

    Escolhendo cores para o site

--- layout/head.php
+++ layout/head.php
@@ -1,3 +1,5 @@
 <head>
   <link rel="stylesheet" href="../assets/styles/global.css">
+  <link rel="stylesheet" href="../assets/styles/colors.css">
+  <link rel="stylesheet" href="../assets/styles/header.css">
 </head>
