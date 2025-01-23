8b73bc6ade42057a95202f25ef2b1aff33699f15
AckinDev <ackinlino123@gmail.com>
2025-01-23 18:55:11 -0300

    Adicionando card de categorias de animais

---
+++ layout/card-animais.php
@@ -0,0 +1,9 @@
+<h1>Categorias de produtos por espécie de animais</h1>
+<div class="container">
+    <div class="box"><img src="../assets/img/cachorro.png" alt=""><span>Cachorro</span></div>
+    <div class="box"><img src="../assets/img/gato.png" alt=""> <span>Gato</span></div>
+    <div class="box"><img src="../assets/img/peixe.png" alt=""> <span>Peixe</span></div>
+    <div class="box"><img src="../assets/img/passaro.png" alt=""> <span>Aves e Pássaros</span></div>
+    <div class="box"><img src="../assets/img/roedor.png" width="80px" alt=""> <span>Coelhos e Roedores</span></div>
+    <div class="box"><img src="../assets/img/reptil.png" alt=""> <span>Répteis</span></div>
+</div>
--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php 
 header('Content-Type: text/html; charset=utf-8');
+session_start();
 
 include_once("../layout/head.php");
 include_once("../layout/header.php");
 include_once("../layout/background-image.php");
 include_once("../layout/carousel.php");
 include_once("../layout/card-box.php");
+include_once("../layout/card-animais.php");
 include_once("../layout/footer.php");
