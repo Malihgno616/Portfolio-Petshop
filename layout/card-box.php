fd58ca5b921bde1e0c12330778b6823066b4eadc
ackindev <ackinlino123@gmail.com>
2025-01-23 13:51:56 -0300

    Add: Imagem para os cards de produtos em destaque

--- layout/card-box.php
+++ layout/card-box.php
@@ -1,9 +1,8 @@
 <h1>Categorias de Produtos em Destaque</h1>
 <div class="container">
-  <div class="box">Banho e Tosa</div>
-  <div class="box">Acessórios</div>
-  <div class="box">Veterinário</div>
-  <div class="box">Ração para Cachorro</div>
-  <div class="box">Ração para Gato</div>
-  <div class="box">Saúde</div>
+  <div class="box"><img src="../assets/img/banho-e-tosa.png" alt=""><span>Banho e Tosa</span></div>
+  <div class="box"><img src="../assets/img/acessorios.png" alt=""> <span>Acessórios</span></div>
+  <div class="box"><img src="../assets/img/veterinario.png" alt=""> <span>Veterinário</span></div>
+  <div class="box"><img src="../assets/img/cachorro.png" alt=""> <span>Ração para Cachorro</span></div>
+  <div class="box"><img src="../assets/img/gato.png" alt=""> <span>Ração para Gato</span></div>  
 </div>
