419a1677cb490d524c2f546e7ecaaa0ec19ad49d
ackindev <ackinlino123@gmail.com>
2025-01-23 12:49:45 -0300

    Add: Carousel de base para o site

--- public/index.php
+++ public/index.php
@@ -4,4 +4,5 @@ header('Content-Type: text/html; charset=utf-8');
 include_once("../layout/head.php");
 include_once("../layout/header.php");
 include_once("../layout/background-image.php");
+include_once("../layout/carousel.php");
 include_once("../layout/card-box.php");
