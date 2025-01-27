66327230b978833e8431a7e64273f1354620de0d
ackindev <ackinlino123@gmail.com>
2025-01-27 13:55:24 -0300

    Adicionando a classe logo no logo principal para não afetar outras imagens

--- layout/background-image.php
+++ layout/background-image.php
@@ -1,5 +1,5 @@
 <div id="imagem-fundo">
     <div>
-        <img src="../assets/img/logo-original-150.jpg" alt="logo">
+        <img class="logo" src="../assets/img/logo-original-150.jpg" alt="logo">
     </div>
 </div>
